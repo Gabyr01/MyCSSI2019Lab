a4a5ede8dc0a677a22749e157a3c503a36a8441e
Gabriela Rojas <rojas124@csusm.edu>
2019-07-31 11:55:49 -0700

    Python for loops and arrays

---
+++ WeLearn/m3-Python/L2-Python_List_Dict/list.py
@@ -0,0 +1,36 @@
+"""
+#ARRAY OR LIST same data structure
+students = ["Alice", "Javi", "Damien"]
+students.append("Delilah") #adding new element to data structure
+print(students)
+
+students.insert(1, "Zoe")
+print(students)
+
+students.remove("Javi") #first one will be removed
+print(students)
+
+for name in smith_siblings:
+    #print(name + " Smith")
+    print(len(smith_siblings))
+
+smith_siblings = ["Emily", "Monique", "Giovanni", "Maria", "Alice", "Javi", "Damien" ]
+
+for index in range(len(smith_siblings)):
+    smith_siblings[index] = smith_siblings[index] + " Smith"
+print(smith_siblings)
+
+superheroes = ["captain Marvel", "Wonder women", "Storm", "Kamala Khan", "Bumblebee", "Jessica Jones"]
+demoted_hero = str(raw_input("What superhero do you want to eliminate from the top 5?"))
+if demoted_hero in superheroes:
+    superheroes.remove(demoted_hero)
+    print("Top 5 heroes", superheroes)
+else:
+    print("Hero not found.")
+"""
+#my_list[start:end:step]
+names = ["Rickon", "Bran", "Arya","Sansa","Jon", "Robb"]
+print(names[::-1]) #['Robb', 'Jon', 'Sansa', 'Arya', 'Bran', 'Rickon']
+print(names[4:2:-1]) #['Jon', 'Sansa']
+print(names[:2]) #end index is not included ['Rickon', 'Bran']
+print(names[::2]) #every other name ['Rickon', 'Arya', 'Jon']
